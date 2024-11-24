69602b5ee9f7384cce48d0536a3264b3b99b823b
Emm4nuell <eduardo.eesf@gmail.com>
2024-11-24 00:31:08 -0300

    Create project Spring Security and JWT

---
+++ src/main/java/br/com/spring_security_tweet/SpringSecurityTweetApplication.java
@@ -0,0 +1,13 @@
+package br.com.spring_security_tweet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityTweetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityTweetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/spring_security_tweet/SpringSecurityTweetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.spring_security_tweet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityTweetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
